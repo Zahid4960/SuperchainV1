645e5cc0e12755db62dcdb07eabfaa0f5c19d040
Zahid4960 <zahidcse4960@gmail.com>
2019-03-18 07:27:31 +0600

    Added registration functionallity and registration form validation

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,78 @@
+  
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* Routes for register for get method */
 router.get('/register', function(req, res, next) {
   res.render('register', {title: 'Register'});
 });
 
-router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard', {title: 'Dashboard'});
+/* Routes for register for post method */
+router.post('/register', upload.single('id_pic'), function(req, res, next) {
+  var role = req.body.role;
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var confirm_password = req.body.confirm_password;
+ // var id_pic = req.file.id_pic;
+
+  /* Check identity picture uploaded or not */
+  if(req.file){
+  	console.log("Uploaded Identity Picture!!!");
+  	var id_pic = req.file.filename;
+  }
+  else{
+  	console.log("Identity Picture is not Uploaded !!! ");
+  	var id_pic = "noImage.jpg";
+  }
+
+  /* Form validation for registration form */
+  req.checkBody('role', 'Role field is required').notEmpty();
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('confirm_password', 'Password do not match').equals(req.body.password);
+  //req.checkfile('id_pic', 'Identity Picture field is required').notEmpty();
+
+  /* Check for errors at the time of submitting registration form */
+  var errors = req.validationErrors();
+
+  if(errors){
+  	res.render('register', {
+  		errors: errors
+  	});
+  }
+  	else{
+  		console.log('No Errors!!!');
+  }
 });
 
+/* Routes for login for get method */
 router.get('/login', function(req, res, next) {
   res.render('login', {title: 'Login'});
 });
 
+/* Routes for dashboard for get method */
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', {title: 'Dashboard'});
+});
+
+/* Routes for contact for get method */
 router.get('/contact', function(req, res, next) {
   res.render('contact', {title: 'Contact'});
 });
 
+/* Routes for about for get method */
 router.get('/about', function(req, res, next) {
   res.render('about', {title: 'About'});
 });
