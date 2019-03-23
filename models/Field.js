9a94b65b614cd7b21f3d9d60a1c95aacbd215405
Zahid4960 <zahidcse4960@gmail.com>
2019-03-23 15:27:04 +0600

    Create database model for field

---
+++ models/Field.js
@@ -0,0 +1,46 @@
+/* Database model named field.js */
+/* For connection with mongodb */
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/SuperchainV1', { useNewUrlParser: true });
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+const db = mongoose.connection;
+
+/* Field schema */
+const FieldSchema = mongoose.Schema({
+	/* For field's product id */
+	productID: {
+		type: String
+	},
+	/* For field's product name */
+	productName:{
+		type: String
+	},
+	/* For field's field location */
+fieldLocation: {
+		type: String
+	},
+	/* For field's farmer name */
+	farmerName: {
+		type: String
+	},
+	/* For field's farmer mobile */
+	farmerMobile: {
+		type: String
+	},
+	/* For field's farmer nid */
+	farmerNid: {
+		type: String
+	},
+	/* For current date */
+	date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+/* For creating new user and save their information into mongodb database(SuperchainV1).
+We will get all the information from registration form*/
+const Field = mongoose.model('Field', FieldSchema);
+module.exports = Field;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
+var async = require('async', true);
+
+const Field = require('../models/Field');
 
 // Index page will redirected to home,navbar barnd(superchain) 
 router.get('/', (req, res) => res.render('index'));
@@ -32,6 +35,7 @@ router.get('/search', (req,res) => res.render('search'));
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('dashboard', {
     user: req.user
+    field: req.field
   })
 );
 
@@ -46,39 +50,56 @@ router.get('/admin', ensureAuthenticated, (req, res) =>
 router.get('/field', ensureAuthenticated, (req, res) =>
   res.render('field', {
     user: req.user
+    field: req.field
   })
 );
 
 
 // field page for post method
 router.post('/field', (req, res) =>{
-  const  { productID, productName, fieldLoaction, farmerName, farmerMobile, farmerNid } = req.body;
-  const errors = [];
+  const  { productID, productName, fieldLocation, farmerName, farmerMobile, farmerNid } = req.body;
+  let errors = [];
 
   //checks for blank field
-  if( !productID || !productName || !fieldLoaction || !farmerName || !farmerMobile || !farmerNid ){
+  if( !productID || !productName || !fieldLocation || !farmerName || !farmerMobile || !farmerNid ){
     errors.push({ msg: 'Please enter all fields' });
   }
   // check for farmer mobile number length if it less than 12 digits then don't accept
-  if (farmerMobile.length < 12) {
-    errors.push({ msg: 'Mobile number must be at least 12 digits' });
-  }
+    if (farmerMobile.length < 11) {
+      errors.push({ msg: 'Mobile number must be at least 11 digits' });
+    }
    // check for farmer nid number length if it less than 17 digits then don't accept
-  if (farmerNid.length < 17) {
-    errors.push({ msg: 'Nid number must be at least 17 digits' });
-  }
-  // if finds any errors then show it
-  if(errors.length > 0){
-    res.render('field',{
-      errors,
-      productID,
-      productName,
-      fieldLoaction,
-      farmerName,
-      farmerMobile,
-      farmerNid
-    });
-  }
+    if (farmerNid.length < 17) {
+      errors.push({ msg: 'Nid number must be at least 17 digits' });
+    }
+    // if finds any errors then show it
+    if(errors.length > 0){
+      res.render('field',{
+        errors,
+        productID,
+        productName,
+        fieldLocation,
+        farmerName,
+        farmerMobile,
+        farmerNid
+      });
+    }
+  // if it passes all the validation then it will store that data into field collections
+    else{
+      const newField = new Field({
+        productID,
+        productName,
+        fieldLocation,
+        farmerName,
+        farmerMobile,
+        farmerNid
+      });
+      newField.save().then( field =>{
+         req.flash('success_msg',
+        'Field data uploaded successfully');
+         res.redirect('/dashboard');
+      });
+    }
 });
 
 // production page for get method
